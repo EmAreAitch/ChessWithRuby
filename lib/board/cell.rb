a275493447c91f21f6433589a872685c48c21b47
emareaitch <dev.emareaitch@gmail.com>
2024-02-14 13:45:06 +0530

    Create 'Cell' class

---
+++ lib/board/cell.rb
@@ -0,0 +1,11 @@
+class Cell 
+	attr_reader :row, :column, :diagonal, :color
+
+	def initialize(options:)
+		@row = options[:row]
+		@column = options[:column]
+		@diagonal = options[:diagonal]
+		@piece ||= options[:piece]
+		@color = options[:color]
+	end
+end
