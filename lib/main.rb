903f89a1c9a0732bad172a6e775b891311c8a7cd
emareaitch <dev.emareaitch@gmail.com>
2024-02-21 21:33:36 +0530

    Display board with pieces

--- lib/main.rb
+++ lib/main.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require_relative 'board/board'
+require_relative 'pieces/piece_controller'
 
 chess_board = Board.new
+piece_controller = PiecesController.new(board: chess_board)
 puts chess_board
